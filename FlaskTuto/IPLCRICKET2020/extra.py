40978608a34b8cf183901d47b64554435f0ff3a5
ravi2545 <mouni.14338@gmail.com>
2020-02-12 15:49:51 +0530

    ADDED FLASK MAIN FILE

---
+++ FlaskTuto/IPLCRICKET2020/extra.py
@@ -0,0 +1,101 @@
+# # import sqlite3
+# #
+# # def readSqliteTable():
+# #     try:
+# #         sqliteConnection = sqlite3.connect('SQLite_Python.db')
+# #         cursor = sqliteConnection.cursor()
+# #         print("Connected to SQLite")
+# #
+# #         sqlite_select_query = """SELECT * from SqliteDb_developers"""
+# #         cursor.execute(sqlite_select_query)
+# #         records = cursor.fetchall()
+# #         print("Total rows are:  ", len(records))
+# #         print("Printing each row")
+# #         for row in records:
+# #             print("Id: ", row[0])
+# #             print("Name: ", row[1])
+# #             print("Email: ", row[2])
+# #             print("JoiningDate: ", row[3])
+# #             print("Salary: ", row[4])
+# #             print("\n")
+# #
+# #         cursor.close()
+# #
+# #     except sqlite3.Error as error:
+# #         print("Failed to read data from sqlite table", error)
+# #     finally:
+# #         if (sqliteConnection):
+# #             sqliteConnection.close()
+# #             print("The SQLite connection is closed")
+# #
+# # readSqliteTable()
+#
+#
+# import sqlite3
+#
+# conn = sqlite3.connect('iplteams.db')
+# print("Opened database successfully")
+#
+# conn.execute('''CREATE TABLE Emp
+#        (ID INT PRIMARY KEY     NOT NULL,
+#        NAME           TEXT    NOT NULL,
+#        AGE            INT     NOT NULL,
+#        ADDRESS        CHAR(50),
+#        SALARY         REAL);''')
+# print("Table created successfully")
+#
+# conn.close()
+#
+# #
+# # import sqlite3
+# #
+# # conn = sqlite3.connect('iplteams.db')
+# # print("Opened database successfully")
+# #
+# # conn.execute("INSERT INTO Employees (ID,NAME,AGE,ADDRESS,SALARY) \
+# # VALUES (1, 'Ajeet', 27, 'Delhi', 20000.00 )");
+# #
+# # conn.execute("INSERT INTO Employees (ID,NAME,AGE,ADDRESS,SALARY) \
+# # VALUES (2, 'Allen', 22, 'London', 25000.00 )");
+# #
+# # conn.execute("INSERT INTO Employees (ID,NAME,AGE,ADDRESS,SALARY) \
+# # VALUES (3, 'Mark', 29, 'CA', 200000.00 )");
+# #
+# # conn.execute("INSERT INTO Employees (ID,NAME,AGE,ADDRESS,SALARY) \
+# # VALUES (4, 'Kanchan', 22, 'Ghaziabad ', 65000.00 )");
+# #
+# # conn.commit()
+# # print("Records inserted successfully"
+# # )
+#
+#
+# # !/usr/bin/python
+#
+# import sqlite3
+#
+# conn = sqlite3.connect('iplteams.db')
+#
+# data = conn.execute("select * from Employees");
+#
+# for row in data:
+#     print("ID = ", row[0])
+#     print("NAME = ", row[1])
+#     print("ADDRESS = ", row[2])
+#     print("SALARY = ", row[3], "\n")
+#
+# conn.close();
+
+l=[('kal', 'Ravi'), ('kal', 'Ravi'), ('kal', 'Ravi'), ('CSK', 'csk123'), ('CSK', 'csk123'), ('CSK', 'csk123')]
+t=('kal', 'Ravi')
+print(type(t))
+# for i in range(0, len(l)):
+#     if l[i]==('C', 'csk123'):
+#         print("YES")
+#         break
+name='ravi'
+pa='csk123'
+t=name, pa,
+tt=t, t
+d=list(tt)
+print(d)
+
--- FlaskTuto/IPLCRICKET2020/mainfile.py
+++ FlaskTuto/IPLCRICKET2020/mainfile.py
@@ -1,18 +1,118 @@
-from flask import Flask
-import pandas
-import json
-app=Flask(__name__)
+from flask import *
+import sqlite3
 
-@app.route('/home/<name>')
-def home(name):
-   return "Hello "+name
+app = Flask(__name__)
 
-@app.route('/home/<int:age>')
-def vote(age):
-    if age>18:
-        return "YOU ARE THE ELIGIBLE FOR VOTE"
-    else:
-        return "NOT ELIGIBLE"
 
-if __name__=="__main__":
+@app.route("/")
+def index():
+    return render_template("index.html");
+
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/savedetails", methods=["POST", "GET"])
+def saveDetails():
+    msg = "msg"
+    if request.method == "POST":
+        try:
+            name = request.form["name"]
+            passw = request.form["psd"]
+            repassw = request.form["repass"]
+            if passw==repassw:
+                with sqlite3.connect("iplteams.db") as con:
+                    cur = con.cursor()
+                    cur.execute("CREATE TABLE IF NOT EXISTS allteams (name VARCHAR(20), password VARCHAR(20))")
+                    cur.execute("INSERT into allteams (name,password) values (?,?)", (name, passw))
+                    con.commit()
+
+            else:
+                msg = "Password is not matching"
+        except:
+            con.rollback()
+            msg = "We can not add the employee to the list"
+        finally:
+            return render_template("success.html", msg=msg)
+            con.close()
+
+# @app.route('/login', methods=["POST"])
+# def login():
+#     if request.method=="POST":
+#         try:
+#             name=request.form["name"]
+#             ps=request.form["ps"]
+#             with sqlite3.connect("iplteams.db") as conn:
+#                 cur = conn.cursor()
+#                 cur.execute("SELECT * FROM allteams")
+#                 rows = cur.fetchall()
+#                 import pdb
+#                 pdb.set_trace()
+#                 conn.commit()
+#                 msg = "Employee successfully Added"
+#         except:
+#             conn.rollback()
+#             msg = "We can not add the employee to the list"
+#
+#         finally:
+#             return render_template("success.html", msg=msg)
+#             conn.close()
+
+
+@app.route("/view", methods=["POST"])
+def view():
+    if request.method == "POST":
+        try:
+            name=request.form["name"]
+            ps=request.form["ps"]
+            with sqlite3.connect("iplteams.db") as conn:
+                cur = conn.cursor()
+                cur.execute("SELECT * FROM allteams")
+                rows = cur.fetchall()
+                tp=name, ps
+                import pdb
+                pdb.set_trace()
+                for i in range(0, len(rows)):
+
+                    pass
+        except:
+            conn.rollback()
+    # con = sqlite3.connect("employee.db")
+    # con.row_factory = sqlite3.Row
+    # cur = con.cursor()
+    # cur.execute("select * from Employees")
+    # rows = cur.fetchall()
+    # return render_template("view.html", rows=rows)
+
+
+@app.route("/delete")
+def delete():
+    return render_template("delete.html")
+
+
+@app.route("/deleterecord", methods=["POST"])
+def deleterecord():
+    id = request.form["id"]
+    with sqlite3.connect("employee.db") as con:
+        try:
+            cur = con.cursor()
+            cur.execute("delete from Employees where id = ?", id)
+            msg = "record successfully deleted"
+        except:
+            msg = "can't be deleted"
+        finally:
+            return render_template("delete_record.html", msg=msg)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
